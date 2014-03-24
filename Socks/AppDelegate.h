113b3673e55f3de5e27a2f19632fa7a9f8da7fc2
Dan Akselrod <dandax@Dans-MacBook-Pro.local>
2014-03-23 22:03:09 -0400

    Initial Commit

---
+++ Socks/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Socks
+//
+//  Created by Dan Akselrod on 3/23/14.
+//  Copyright (c) 2014 dantap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
